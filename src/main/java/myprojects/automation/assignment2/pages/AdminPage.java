6af0570080d280e36189916c8b3a3e429c7c73b8
w8forme <velesgp@mail.ru>
2017-11-15 14:05:07 +0200

    Login and logout done

---
+++ src/main/java/myprojects/automation/assignment2/pages/AdminPage.java
@@ -0,0 +1,7 @@
+package myprojects.automation.assignment2.pages;
+
+/**
+ * Created by veles on 15.11.2017.
+ */
+public class AdminPage {
+}
--- src/main/java/myprojects/automation/assignment2/tests/LoginTest.java
+++ src/main/java/myprojects/automation/assignment2/tests/LoginTest.java
@@ -4,22 +4,27 @@ import myprojects.automation.assignment2.BaseScript;
 import myprojects.automation.assignment2.pages.LoginPage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginTest extends BaseScript {
 
     public static void main(String[] args) throws InterruptedException {
         // TODO Script to execute login and logout steps
         WebDriver driver = getDriver();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
         LoginPage loginPage = new LoginPage(driver);
+        By exitBtn =By.id("header_logout");
+        By userIcon = By.id("employee_infos");
 
         loginPage.openLoginPage();
         loginPage.fillEmailInput();
         loginPage.fillPassInput();
         loginPage.clickLoginBtn();
-        Thread.sleep(4000);
-        driver.findElement(By.id("employee_infos")).click();
-        Thread.sleep(4000);
-        driver.findElement(By.id("header_logout")).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(userIcon));
+        driver.findElement(userIcon).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(exitBtn));
+        driver.findElement(exitBtn).click();
         driver.quit();
     }
 }
